8e57a102435346371ca53a62cf4cbe20f08ef008
Steven Core <42Echo6Alpha@gmail.com>
2016-06-18 14:07:47 +1000

    Created simple_plugin

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # simple_plugin
-A simple class for importing and running py files.
+A simple class for loading and running external python files.
+Useful for a plugin or command system.
+
+## Usage
+
+### Loading plugins
+```python3
+import simple_plugin
+
+plugins = simple_plugin.Plugins('folder/of/plugins/')
+```
+Thats all it takes to load a folder of plugins.
+
+### Running plugins
+```python3
+import simple_plugin
+
+plugins = simple_plugin.Plugins('folder/of/plugins/')
+plugins['file_name'].function()
+```
+Where file_name.py is a file in the folder/of/plugins/ folder.
+This code will run function() from file_name.py
+
+**note:**
+The file name will have .py at the end but you don't run it with .py
+Just use the name without the extension.
+
+### Iterating over the plugins
+The plugin class supports iteration.
+```python3
+import simple_plugin
+
+plugins = simple_plugin.Plugins('folder/of/plugins/')
+for name, ref in plugins:
+    print(name)
+    ref.function()
+```
+This will print the name of each plugin and run .function() from each plugin.
---
+++ simple_plugin.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+"""simple_plugin
+A class for easily importing and calling external python files.
+"""
+
+import os
+import sys
+
+
+class Plugins:
+    def __init__(self, folder, load_on_start=True):
+        """__init__
+        params:
+            folder: str: The folder to load the plugins from.
+            load_on_start: bool:
+                True: Will load the plugins in __init__
+                False: Will not load the plugins until the user calls
+                       .load_plugins()
+        """
+        if not os.path.exists(folder):
+            raise OSError('Path does not exist: {}'.format(folder))
+        if not folder.endswith('/'):
+            folder += '/'
+        self.folder = folder
+        if load_on_start:
+            self.load_plugins()
+
+    def load_plugins(self, extentions=['.py']):
+        """load_plugins
+        Loads the plugins from the folder specified in __init__
+        params:
+            extentions: list: A list of file extensions to include.
+                              Defaults to .py files. It will attempt to load
+                              them as a Python module no matter what file
+                              extention it has.
+        note: This method will temporarily move the working directory.
+              It will move it back when it's done.
+        """
+        self.plugins = {}
+        current_path = os.getcwd()
+        sys.path.insert(0, self.folder)
+        for name in os.listdir(self.folder):
+            if (not any(name.endswith(ext) for ext in extentions) or
+                    os.path.isdir(self.folder + name)):
+                continue
+            fname = '.'.join(name.split('.')[:-1])
+            self.plugins[fname] = __import__('{}'.format(fname))
+        sys.path.insert(0, current_path)
+        return None
+
+    def import_func(self, name, func):
+        """import_func
+        Retrieves a certain function from a plugin.
+        params:
+            name: str: The name of the plugin to load from.
+            func: str: The name of the function to load.
+        return: A reference to the function if it exists, otherwise it
+                returns None.
+        """
+        if name in self.plugins:
+            return getattr(self.plugins[name], func, None)
+        return None
+
+    def __getattr__(self, item):
+        if item in self.plugins:
+            return self.plugins[item]
+        raise AttributeError('Plugin could not be found: "{}"'.format(item))
+
+    def __getitem__(self, item):
+        return self.__getattr__(item)
+
+    def __iter__(self):
+        for plg in self.plugins:
+            yield (plg, self.plugins[plg])
+        raise StopIteration
